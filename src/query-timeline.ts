501d1908e62723940733d6d00a724c49c98787c2
Palani Ramanathan <pramanathan@pramana-ltmdp9b.internal.salesforce.com>
2022-10-02 19:24:29 +0530

    Minor fixes

--- src/query-timeline.ts
+++ src/query-timeline.ts
@@ -21,7 +21,7 @@ export class QueryTimeline {
       content: s.getStageId(),
       start: s.getStartTime(),
       end: s.getEndTime(),
-      title: s.getStageId(),
+      title: s.getStartTime() + "," + s.getEndTime(),
     };
     dataItems.push(dataItem);
 
@@ -31,7 +31,8 @@ export class QueryTimeline {
         content: t.getTaskId(),
         start: t.getStartTime(),
         end: t.getEndTime(),
-        title: t.getTaskId(),
+        title: t.getStartTime() + "," + t.getEndTime(),
+        className: 'green',
       };
       dataItems.push(dataItem)
     });
@@ -48,7 +49,7 @@ export class QueryTimeline {
 
   private processStage(s: any, level: number): Stage {
     let stage: Stage = new Stage(s.stageId, level);
-    queryJson.outputStage.tasks.forEach((t) => {
+    s.tasks.forEach((t: any) => {
       const task: Task = new Task(
         t.taskStatus.taskId,
         new Date(t.stats.firstStartTime),
@@ -78,7 +79,7 @@ export class QueryTimeline {
     let maxNumber = this.getDataItems(q, dataItems);
 
     const levelGroups: LevelGroup[] = [];
-    for (let i = 0; i < maxNumber; ++i) {
+    for (let i = 0; i <= maxNumber; ++i) {
       let newLevel: LevelGroup = {
         id: i,
         content: 'Level ' + i,
@@ -86,59 +87,6 @@ export class QueryTimeline {
       };
       levelGroups.push(newLevel);
     }
-
-    // const groups = new DataSet([
-    //   { id: 0, content: 'First', value: 1 },
-    //   { id: 1, content: 'Third', value: 3 },
-    //   { id: 2, content: 'Second', value: 2 },
-    // ]);
-
-    // create a dataset with items
-    // note that months are zero-based in the JavaScript Date object, so month 3 is April
-    const items = new DataSet([
-      {
-        id: 0,
-        group: 0,
-        content: 'item 0',
-        start: new Date(2014, 3, 17),
-        end: new Date(2014, 3, 21),
-      },
-      {
-        id: 1,
-        group: 0,
-        content: 'item 1',
-        start: new Date(2014, 3, 19),
-        end: new Date(2014, 3, 20),
-      },
-      {
-        id: 2,
-        group: 1,
-        content: 'item 2',
-        start: new Date(2014, 3, 16),
-        end: new Date(2014, 3, 24),
-      },
-      {
-        id: 3,
-        group: 1,
-        content: 'item 3',
-        start: new Date(2014, 3, 23),
-        end: new Date(2014, 3, 24),
-      },
-      {
-        id: 4,
-        group: 1,
-        content: 'item 4',
-        start: new Date(2014, 3, 22),
-        end: new Date(2014, 3, 26),
-      },
-      {
-        id: 5,
-        group: 2,
-        content: 'item 5',
-        start: new Date(2014, 3, 24),
-        end: new Date(2014, 3, 27),
-      },
-    ]);
 
     // create visualization
     const container: HTMLElement | null = document.getElementById('timeline');
